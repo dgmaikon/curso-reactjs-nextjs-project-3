02d1ef3b9e5c5ab5634b8570ed9a024c38ad8316
dgmaikon <dgmaikondev@gmail.com>
2023-11-14 14:29:15 -0300

    style: change hex colors

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,8 +1,9 @@
 export const theme = {
   
   colors: {
-    mainBg:'red',
-    secondaryBg:' blue'
+    primaryColor: '#0A1128',
+    secondaryColor: '#dc143c',
+    white: '#FFFFFF',
   },
   
   fonts: {
