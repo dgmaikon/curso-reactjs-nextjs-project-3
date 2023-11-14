812301e5120726ea554efddf1f189994652aa2e0
dgmaikon <dgmaikondev@gmail.com>
2023-11-14 14:28:46 -0300

    style: change background colors

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -6,13 +6,26 @@ import { theme } from '../src/styles/theme'
 const preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
+    backgrounds: {
+      default: 'light',
+      values: [
+        {
+          name: 'light',
+          value: theme.colors.white,
+        },
+        {
+          name: 'dark',
+          value: theme.colors.primaryColor,
+        },
+      ]
+    }
+  },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
     },
-  },
 };
 
 export const decorators = [
