036d606599f49a2b419e5320c781755a0a342625
dgmaikon <dgmaikondev@gmail.com>
2023-11-14 13:45:39 -0300

    feat: remove style from component

--- src/templates/App/styles.js
+++ src/templates/App/styles.js
@@ -1,9 +1,9 @@
 import styled, {css} from 'styled-components';
 
 export const Wrapper = styled.div`
-${(props) => css`
-  background: ${props.background};
-  `}
+  /* ${(props) => css`
+    background: ${props.background};
+    `} */
 `;
 
 //destructuring
